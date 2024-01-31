d84b8adbed0e7653a85fe5da598b8d218eddf3a1
Emersonmrbr <54491594+Emersonmrbr@users.noreply.github.com>
2024-01-31 16:58:24 -0300

    add edit game

--- GameStore.Client/GameClient.cs
+++ GameStore.Client/GameClient.cs
@@ -13,14 +13,14 @@ public static class GameClient
         ReleaseDate = new DateTime(1991,2,1)
         },
         new Game(){
-        Id = 1,
+        Id = 2,
         Name = "Final Fantasy XIV",
         Genre = "Roleplaying",
         Price = 59.99M,
         ReleaseDate = new DateTime(2010,9,30)
         },
         new Game(){
-        Id = 1,
+        Id = 3,
         Name = "FIFA 23",
         Genre = "Sports",
         Price = 69.99M,
@@ -31,5 +31,24 @@ public static class GameClient
     public static Game[] GetGames()
     {
         return games.ToArray();
+    }
+
+    public static void AddGame(Game game)
+    {
+        game.Id = games.Max(game => game.Id) + 1;
+        games.Add(game);
+    }
+    public static Game GetGame(int id)
+    {
+        return games.Find(game => game.Id == id) ?? throw new Exception("Could not find game!");
+    }
+    public static void UpdatGame(Game updateGame)
+    {
+        Game existingGame = GetGame(updateGame.Id);
+        existingGame.Name = updateGame.Name;
+        existingGame.Genre = updateGame.Genre;
+        existingGame.Price = updateGame.Price;
+        existingGame.ReleaseDate = updateGame.ReleaseDate;
+
     }
 }
